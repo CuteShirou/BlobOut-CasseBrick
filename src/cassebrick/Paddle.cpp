590a33d3c90eae362186df0a53a71ec4b20b7fb1
Nyevius <130132864+Nyevius@users.noreply.github.com>
2024-10-28 12:53:35 +0100

    [EDIT] Start code Paddle

--- src/cassebrick/Paddle.cpp
+++ src/cassebrick/Paddle.cpp
@@ -1,2 +1,42 @@
 #include "pch.h"
 #include "Paddle.h"
+#include <SFML/Graphics.hpp>
+
+Paddle::Paddle(sf::Vector2<float> pos_) : pos(pos_)
+{
+}
+
+Paddle::~Paddle()
+{
+}
+
+void Paddle::Destroy()
+{
+}
+
+void Paddle::Move()
+{
+}
+
+bool Paddle::OnCollision(Entity& entity)
+{
+	return false;
+}
+
+sf::Vector2<float> Paddle::GetPos()
+{
+	return pos;
+}
+
+void Paddle::SetPos(sf::Vector2<float> pos_)
+{
+	pos = pos_;
+}
+
+sf::Sprite Paddle::SpriteDraw(std::string imgDirectory)
+{
+	sf::Texture texture;
+	texture.loadFromFile(imgDirectory);
+	sf::Sprite sprite(texture);
+	return sprite;
+}
--- src/cassebrick/Paddle.h
+++ src/cassebrick/Paddle.h
@@ -1,5 +1,22 @@
 #pragma once
-class Paddle
+#include <SFML/Graphics.hpp>
+#include "Entity.h"
+#include <iostream>
+
+class Paddle : public Entity
 {
+public:
+	Paddle(sf::Vector2<float> pos);
+	~Paddle();
+	void Destroy() override;
+	void Move() override;
+	bool OnCollision(Entity& entity) override;
+	sf::Vector2<float> GetPos() override;
+	void SetPos(sf::Vector2<float> pos) override;
+	sf::Sprite SpriteDraw(std::string imgDirectory) override;
+
+protected:
+	sf::Vector2<float> pos;
+	sf::Sprite sprite;
 };
 
