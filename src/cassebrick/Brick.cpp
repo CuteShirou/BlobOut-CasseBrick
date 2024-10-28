3640b964bfc11bf6604bf929d6a73df4ffed87d3
Valentin <111918150+CuteShirou@users.noreply.github.com>
2024-10-28 11:29:53 +0100

    [NEW] Brick CPP + H

--- src/cassebrick/Brick.cpp
+++ src/cassebrick/Brick.cpp
@@ -1,2 +1,54 @@
 #include "pch.h"
 #include "Brick.h"
+
+Brick::Brick(sf::Vector2<float> posBrick, sf::Vector2<float> size) : posBrick(posBrick), m_IsDestroyed(false)
+{
+	brick.setSize(size);
+	brick.setPosition(posBrick);
+	brick.setFillColor(sf::Color::Red);
+}
+
+Brick::~Brick()
+{
+}
+
+sf::RectangleShape Brick::GetBrick()
+{
+	return brick;
+}
+
+void Brick::Destroy()
+{
+	m_IsDestroyed = true;
+	brick.setFillColor(sf::Color::Transparent);
+}
+
+bool Brick::IsDestroyed() const
+{
+	return m_IsDestroyed;
+}
+
+bool Brick::CheckCollision(const sf::CircleShape& ball) {
+    if (m_IsDestroyed) return false;
+
+    // Coordonnées de la balle
+    sf::FloatRect ballBounds = ball.getGlobalBounds();
+
+    // Coordonnées de la brique
+    sf::FloatRect brickBounds = brick.getGlobalBounds();
+
+    // Vérifie si la balle intersecte la brique
+    if (brickBounds.intersects(ballBounds)) {
+        Destroy(); // Casse la brique en cas de collision
+        return true;
+    }
+    return false;
+}
+
+void Brick::Draw(sf::RenderWindow& window)
+{
+	if (m_IsDestroyed)
+	{
+		window.draw(brick);
+	}
+}
--- src/cassebrick/Brick.h
+++ src/cassebrick/Brick.h
@@ -1,5 +1,23 @@
-#pragma once
+#ifndef BRICK_H__
+#define BRICK_H__
+
+#include <SFML/Graphics.hpp>
+#include <vector>
+
 class Brick
 {
+private:
+	sf::RectangleShape brick;
+	bool m_IsDestroyed;
+	sf::Vector2<float> posBrick;
+public:
+	Brick(sf::Vector2<float> posBrick, sf::Vector2<float> size);
+	~Brick();
+	sf::RectangleShape GetBrick();
+	void Destroy();
+	bool IsDestroyed() const;
+	bool CheckCollision(const sf::CircleShape& Ball);
+	void Draw(sf::RenderWindow& window);
 };
 
+#endif // BRICK_H__
