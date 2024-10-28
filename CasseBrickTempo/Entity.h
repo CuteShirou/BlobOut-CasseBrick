f1f47b596723678ef5272499615d602b3d3f39ab
poplouf <pferran@gaming.tech>
2024-10-28 09:33:17 +0100

    add class

--- CasseBrickTempo/Entity.h
+++ CasseBrickTempo/Entity.h
@@ -1,5 +1,17 @@
-#pragma once
+#ifndef ENTITY_H__
+#define ENTITY_H__
+
 class Entity
 {
+public:
+	Entity();
+	~Entity();
+	virtual void destroy() = 0
+
+private:
+
 };
+
+
+#endif // !ENTITY_H__
 
