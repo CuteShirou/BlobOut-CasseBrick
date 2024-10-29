dab4d9cbb31d7d318ec4f3a542c108ddd4913e4e
Valentin <111918150+CuteShirou@users.noreply.github.com>
2024-10-29 09:45:31 +0100

    [NEW] Window CPP + H

--- src/cassebrick/Window.cpp
+++ src/cassebrick/Window.cpp
@@ -1,2 +1,60 @@
 #include "pch.h"
 #include "Window.h"
+
+
+Window::Window()
+{
+
+}
+
+Window::~Window()
+{
+
+}
+
+void Window::CreateWindow(int width, int height)
+{
+
+	window.create(sf::VideoMode(width, height), "*test*");
+}
+
+void Window::Clear()
+{
+	window.clear(sf::Color::Black);
+}
+
+void Window::Display()
+{
+	window.display();
+}
+
+
+void Window::PollEvents()
+{
+	sf::Event event;
+	while (window.pollEvent(event))
+	{
+		if (event.type == sf::Event::Closed)
+			window.close();
+	}
+}
+
+bool Window::IsRunning()
+{
+	return window.isOpen();
+}
+
+void Window::Close()
+{
+	window.close();
+}
+
+void Window::Draw(sf::Sprite sprite)
+{
+	window.draw(sprite);
+}
+
+sf::RenderWindow& Window::GetWindow()
+{
+	return window;
+}
--- src/cassebrick/Window.h
+++ src/cassebrick/Window.h
@@ -1,5 +1,28 @@
-#pragma once
+#ifndef WINDOW_H__
+#define WINDOW_H__
+
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
 class Window
 {
+public:
+	Window();
+	~Window();
+	void CreateWindow(int width, int height);
+	void Clear();
+	void Display();
+	void PollEvents();
+	bool IsRunning();
+	void Close();
+	void Draw(sf::Sprite sprite);
+	sf::RenderWindow& GetWindow();
+
+
+protected:
+	sf::RenderWindow window;
+
 };
 
+
+#endif // !WINDOW_H__
