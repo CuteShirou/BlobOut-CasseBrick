a66fbc189bc2e66471b7b56ce635d2f12f0db467
Valentin <111918150+CuteShirou@users.noreply.github.com>
2024-10-29 09:46:44 +0100

    [NEW] Main

--- src/cassebrick/main.cpp
+++ src/cassebrick/main.cpp
@@ -1,8 +1,27 @@
 #include "pch.h"
 #include "main.h"
+#include "Window.h"
+#include "Paddle.h"
+#include "Brick.h"
+
+#include<iostream>
+
 
 int main()
 {
+	Window window;
+	Brick brick(sf::Vector2<float>(100, 100), sf::Vector2<float>(100, 100), sf::Color::Red);
+
+	window.CreateWindow(800, 600);
+	while (true) {
+		window.PollEvents();
+		window.Clear();
+		sf::Sprite sprite = brick.SpriteDraw("../../../src/cassebrick/BRICK.png");
+		window.Draw(sprite);
+		window.Display();
+		
+	}
+
 #ifdef _DEBUG
 	_CrtDumpMemoryLeaks();
 #endif
