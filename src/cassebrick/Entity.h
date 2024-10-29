46d1913229623e48d34532110001243a2347e16e
Valentin <111918150+CuteShirou@users.noreply.github.com>
2024-10-29 09:46:05 +0100

    [NEW/EDIT] NEW Entity.cpp + rework Entity.h

--- src/cassebrick/Entity.h
+++ src/cassebrick/Entity.h
@@ -1,21 +1,26 @@
 #ifndef ENTITY_H__
 #define ENTITY_H__
 
+#include <iostream>
+#include <vector>
+#include <SFML/Graphics.hpp>
+
 class Entity
 {
 public:
 	Entity();
 	~Entity();
-	virtual void destroy() = 0;
-	virtual void move() = 0;
-	virtual bool onCollision(Entity& entity) = 0;
-	virtual
+	virtual void Destroy() = 0;
+	virtual void Move() = 0;
+	virtual bool OnCollision(Entity& entity) = 0;
+	virtual sf::Vector2<float> GetPos() = 0;
+	virtual void SetPos(sf::Vector2<float> pos) = 0;
+	virtual sf::Sprite SpriteDraw(std::string imgDirectory) = 0;
 
-
-private:
+protected:
+	sf::Vector2<float> pos;
 
 };
 
 
 #endif // !ENTITY_H__
-
