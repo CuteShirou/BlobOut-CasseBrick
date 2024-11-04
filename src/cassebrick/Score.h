54d40dc60ceeb2c0c0a013c62c738b65f8d9f48c
Valentin <111918150+CuteShirou@users.noreply.github.com>
2024-11-04 13:02:18 +0100

    [NEW] Score + Font

---
+++ src/cassebrick/Score.h
@@ -0,0 +1,28 @@
+#ifndef SCORE_H
+#define SCORE_H
+
+#include <SFML/Graphics.hpp>
+#include <string>
+
+class Score {
+protected:
+    int score;                    // Variable pour stocker le score actuel
+    sf::Text scoreText;           // Texte pour afficher le score
+    sf::Font font;                // Police pour le texte du score
+
+
+public:
+    // Constructeur
+    Score();
+
+    // Méthodes pour gérer le score
+    void Reset();                 // Remet le score à zéro
+    void Increase(int points);    // Augmente le score d'un certain nombre de points
+	sf::Text GetScoreText();      // Getter pour obtenir le texte du score
+	void SetTextPos(sf::Vector2f pos); // Setter pour définir la position du texte (scoreText.setPosition(pos);)
+
+    // Getter pour obtenir le score actuel
+    int GetScore() const;
+};
+
+#endif // SCORE_H
