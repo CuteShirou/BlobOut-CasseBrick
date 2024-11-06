2164dc9dec0f1a3c221be7bb99462ec3474dd734
Nyevius <130132864+Nyevius@users.noreply.github.com>
2024-11-06 16:41:19 +0100

    [NEW] Classe menu

---
+++ src/cassebrick/Menu.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include "Window.h"
+
+class Menu {
+    int pos;
+    int gameState = 0;
+    bool pressed, theselect;
+
+    sf::RectangleShape* winclose;
+    sf::Font* font;
+    sf::Texture* image;
+    sf::Sprite* bg;
+
+    sf::Vector2i pos_mouse;
+    sf::Vector2f mouse_coord;
+
+    std::vector<const char*> options;
+    std::vector<sf::Vector2f> coords;
+    std::vector<sf::Text> texts;
+    std::vector<std::size_t> sizes;
+
+protected:
+    void SetValues();
+    void LoopEvents(Window& window);
+    void DrawAll(Window& window);
+
+public:
+    Menu();
+    ~Menu();
+    void RunMenu(Window& window);
+    int GetState();
+};
